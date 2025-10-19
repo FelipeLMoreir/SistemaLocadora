4b68ae350bdaf74d92c9315ebec453995f51e0be
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-10-19 13:17:39 -0300

    Add project files.

---
+++ SistemaLocadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using SistemaLocadora;
+
+int opcao;
+
+do
+{
+    Console.WriteLine("Digite qual opção dseja:\n1 - Cadastrar Veículo" +
+        "\n2 - Cadastrar Cliente\n3 - Registro e valor da locação" +
+        "\n4 - Listar veículos, clientes e locações");
+    opcao = int.Parse(Console.ReadLine()!);
+    switch (opcao)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+    }
+}
+while (opcao != 0);
